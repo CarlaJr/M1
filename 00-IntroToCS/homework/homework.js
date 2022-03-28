716b80b8805bb7208783b4df715e05f237c92708
CarlaJr <carla_sofia_jr@outlook.com>
2022-03-28 14:29:03 -0500

    HW_001m

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,7 +2,15 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-var suma = 0;
+  var array = num.split("").reverse();
+  var suma = 0;
+  for(var i = 0; i < array.length;i++){
+    var x = array[i];
+    suma = suma + x*Math.pow(2,i);
+  }
+  return suma;
+  
+/* var suma = 0;
 var e = 0;
 for(var i = num.length-1;i >=0 ;i--){
   var x = true;
@@ -14,25 +22,45 @@ for(var i = num.length-1;i >=0 ;i--){
     x = false;
   }
 }
-return suma;
+return suma; */
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-var i = 0;
-var array = [];
-while(num >= 1){
-  i++;
-  var x = num%2;
-  array.push(Math.floor(x));
-  num = num/2;
-}
-var base = array.join("");
-var invertir = "";
-for (var i = base.length - 1; i >= 0 ;i--){
-  invertir = invertir + base.charAt(i);
-}
-return invertir;
+  // tu codigo aca :
+  var i = 0;
+  var array = [];
+  while(num >= 1){
+    i++;
+    var x = num%2;
+    array.push(Math.floor(x));
+    num = num/2;
+  }
+  return array.reverse().join("");
+
+  /* var i = 0;
+  var array = [];
+  while(num >= 1){
+    i++;
+    var x = num%2;
+    array.unshift(Math.floor(x));
+    num = num/2;
+  }
+  return array.join(""); */
+
+  /* var i = 0;
+  var array = [];
+  while(num >= 1){
+    i++;
+    var x = num%2;
+    array.push(Math.floor(x));
+    num = num/2;
+  }
+  var base = array.join("");
+  var invertir = "";
+  for (var i = base.length - 1; i >= 0 ;i--){
+    invertir = invertir + base.charAt(i);
+  }
+  return invertir; */
 }
 
 

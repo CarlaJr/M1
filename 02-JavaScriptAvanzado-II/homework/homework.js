37654b06938ea0b304ae902ab0317663dd9b9b3e
CarlaJr <carla_sofia_jr@outlook.com>
2022-03-31 08:11:18 -0500

    HW_003c

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -1,19 +1,28 @@
 'use strict'
 
 function counter() {
+//Si colocaba la variable count en el global context sí funciona, 
+//pero solo podre usar UN COUNTER, ya que sigue pisando a la variable global
   // Retorna una funcion que cuando sea invocada retorne un valor creciente.
   // el primer valor deberia ser 1.
   // Vas a tener que usar closures.
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return function(){
+    count = count + 1;
+    return count;
+  }
 }
 
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
   // la función que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
-  // cuando la función que hayas retornado es invocada de nuevo, debería guardar el argumento y el resultado de la invocacion
-  // cuando la función que retornaste sea invocada de nuevo con un argumento con el cual se había invocado anterioremente, no deberia invocar de nuevo a cb
+  // cuando la función que hayas retornado es invocada de nuevo, debería guardar el argumento y el resultado 
+  //de la invocacion
+  // cuando la función que retornaste sea invocada de nuevo con un argumento con el cual se había 
+  // invocado anterioremente, no deberia invocar de nuevo a cb
   // debería retornar el resultado (previamente guardado)
   // Ejemplo:
   // cb -> function(x) { return x * x; }
@@ -21,6 +30,20 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var obj = {}
+  return function(x){
+    if(obj.hasOwnProperty(x)){
+      return obj[x];
+    }
+    obj[x] = cb(x);
+    return obj[x];
+  }
+  /* var obj = {};
+  return function(x){
+    if(obj.hasOwnProperty(x)) obj[x];
+    else obj[x] = cb(x);
+    return obj[x];
+  } */
 }
 
 // Bind
@@ -41,8 +64,8 @@ function getNombre(){
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -61,13 +84,19 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(this,'*','*'); // Este this, hace referencia al obj global
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(undefined,'-','-'); //Siempre tenemos que pasar el primer argumento al bindear
 
-let textoUnderscore = undefined;
+let textoUnderscore = crearCadena.bind(null,'_','_'); //Y puede ser undefined o null
 
+//Siempre tenemos que pasar el primer argumento al bindear, porque este es una referencia para crear el vinculo 
+//(bind = unión) de donde sacaremos una copia de nuestra referencia. 
+//Cuando no tenemos una referencia podemos usar null, undefined o el mismo this.
 
+//Function currying
+//Despues de que bind acepta su primer parametro, los siguientes que recibe sirven para bindear o unir los 
+//parametros de la función y estos se vuelven los valores fijos (para modificalos es directamente sobre bind)
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
